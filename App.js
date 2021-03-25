ce72a2a5672fbf0a1e3e7718ab0a4842af4ef48c
mauv2sky <mauv2sky@gmail.com>
2021-03-25 23:14:09 +0900

    Add loading activity code

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import * as Location from "expo-location";
+import { Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  }
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+    } catch (error) {
+      Alert.alert("We can't find you", "so sad");
+    }
+    
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const {isLoading} = isLoading;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
